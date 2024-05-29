67edc3514a25182a1309009d8d84a40133291da2
Krishna Dilip <dilip.adhurs@gmail.com>
2024-05-29 15:27:41 +0530

    Added a model

---
+++ models/wk_addresses.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized='table',
+) }}
+
+WITH wk_addresses AS (
+    SELECT DISTINCT
+        EMPLID AS "Employee ID",
+        COUNTRY AS "Country ISO Code",
+        ADDRESS1 AS "Address Line #1",
+        ADDRESS2 AS "Address Line #2",
+        CITY AS "Municipality",
+        "STATE" AS "Region",
+        POSTAL AS "Postal Code"
+    FROM
+        ORACLE_TEST.PS_ADDRESSES
+    WHERE address_type IN ('HOME')
+),
+wk_addresses1 AS (
+    SELECT DISTINCT
+	    NULL AS "Employee ID",
+        COUNTRY AS "Country ISO Code",
+        ADDRESS1 AS "Address Line #1",
+        ADDRESS2 AS "Address Line #2",
+        CITY AS "Municipality",
+        "STATE" AS "Region",
+        POSTAL AS "Postal Code"
+     FROM
+        ORACLE_TEST.PS_ADDRESSES
+     WHERE address_type IN ('MAIL', 'OTH', 'PERM', 'LEGL')
+ )
+SELECT * FROM wk_addresses
+UNION ALL
+SELECT * FROM wk_addresses1;
